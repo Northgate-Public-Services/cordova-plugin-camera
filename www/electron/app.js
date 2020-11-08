394c829fcf218e6c919690d61c85e1b0758b0b6b
amin.nps <amin.ahamdi@northgateps.com>
2020-11-08 20:55:54 +0000

    added electron to 4.0.x branch

---
+++ www/electron/app.js
@@ -0,0 +1,41 @@
+// Set constraints for the video stream
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints/facingMode
+// facingMode: "environment" or "user"
+var constraints = { video: { facingMode: "environment" }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    //cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    // cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    // cameraOutput.classList.add("taken");
+    // track.stop();
+    var imageData = cameraSensor.toDataURL('image/jpeg');
+    //imageData = imageData.replace('data:image/jpeg;base64,', '');
+    sessionStorage.setItem('cordova-plugin-camera-picture-base64', imageData);
+    sessionStorage.setItem('cordova-plugin-camera-picture-isset', 'true');
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
